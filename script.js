8215e7ebb6ea27fe904e49101a29e8e8120821b6
Alexandre <veladorisz@gmail.com>
2023-01-17 23:28:52 -0300

    logica incompleta

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-var btn = document.getElementById('nome');
-function pegarText(){
- 
+function pegaPokemons(url){
+     fetch(`https://pokeapi.co/api/v2/pokemon/`+url)
+     .then(response=> response.json())
+     .then(pokemonSingle =>{
+             var pokemons = [];
+         pokemons.push({nome:pokemonSingle.name, imagem:pokemonSingle.sprites.front_default, tipo:pokemonSingle.types.type});               
+             var pokeBox = document.querySelector('.card__content');
+             pokeBox.innerHTML = "";
+             pokemons.map((val)=>{   
+             pokeBox.innerHTML+= `
+               <div class="card__content">
+             <h1>${val.nome}</h1>
+                 <img src="${val.imagem}" alt="" srcset="">
+                 <p>eletric</p>
+         </div>
+             `
+             })
+     });
 }
+var nome = document.getElementById("xxt");
+nome.addEventListener('keyup',()=>{
+ pegaPokemons(nome.value);
+})
 
+// function Enviar() {
 
+//     var nome = document.getElementById("xxt");
 
+//     if (nome.value != "") {
+//         pegaPokemons(nome.value);
+//     }
 
-
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-.then(response=> response.json())
-.then(allpokemon => {
-    var pokemons = [];
-
-    allpokemon.results.map((val)=>{
-        
-        fetch(val.url)
-        .then(response => response.json())
-        .then(pokemonSingle =>{
-            pokemons.push({nome:val.name, imagem:pokemonSingle.sprites.front_default});
-            if(pokemons.length==10){
-                pokemons.map((val)=>{
-                    btn.addEventListener('click',()=>{
-                        console.log(val.nome)
-                    })
-                })
-            };
-        })
-
-
-    })
-});
+// }
